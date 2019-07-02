886e94e6afefcf03d844088fc87b1252fd7d591a
ANDRIEL123 <andrielmaicom@hotmail.com>
2019-07-01 23:59:46 -0300

    curso em video js

--- Curso em video - JavaScript/Exercicios js/Exercicio 1/code.js
+++ Curso em video - JavaScript/Exercicios js/Exercicio 1/code.js
@@ -1,1 +1,58 @@
-alert('ola')
+function formataHora(time) {
+    if(time < 10) {
+        return '0'+String(time)
+    } else {
+        return time
+    }
+}
+
+function time() {
+    var today = new Date()
+    var h = today.getHours()
+    hformatado = formataHora(h)
+    var m = today.getMinutes() //ESCOPO GLOBAL VAR
+    mformatado = formataHora(m)
+    var s = today.getSeconds()//ESCOPO GLOBAL VAR 
+    sformatado = formataHora(s)
+    var hora = document.getElementById('horaatual') //ESCOPO GLOBAL VAR
+    hora.innerHTML = hformatado+":"+mformatado+":"+sformatado
+    verificaPeriodo(h, m, s)
+    setTimeout('time()',500)
+}
+
+function verificaPeriodo(h, m, s) {
+    let hora = document.getElementById('periodo')
+    let imagem = document.getElementById('imgfundo')
+    if (h > 5 && h < 12) {
+        if (h == 6 && m == 0 & s ==0) {
+            location.reload()
+        }
+        hora.innerHTML = 'Agora é manhã!'
+        document.body.style.background = "url('_imagens/manha.jpg')"
+        imagem.src = "_imagens/bomdia.jpg"
+    } else if (h > 11 && h < 14) {
+        if (h == 12 && m == 0 & s ==0) {
+            location.reload()
+        }
+        hora.innerHTML = 'Agora é de meio dia!'
+        document.body.style.background = "url('_imagens/meiodia.jpg')"
+        imagem.src = "_imagens/bommeiodia.jpg"
+    } else if (h > 13 && h < 19) {
+        if (h == 14 && m == 0 & s ==0) {
+            location.reload()
+        }
+        hora.innerHTML = 'Agora é de tarde!'
+        document.body.style.background = "url('_imagens/tarde.jpg')";
+        imagem.src = "_imagens/boatarde.jpg"
+        
+    } else {
+        if(h== 18 && m == 0 && s == 0){
+            location.reload()
+        }
+        hora.innerHTML = 'Agora é noite!'
+        document.body.style.background = "url('_imagens/noite.jpg')"
+        imagem.src = "_imagens/boanoite.jpg"
+    }
+
+    
+}
---
+++ Curso em video - JavaScript/Exercicios js/Exercicio 2/code.js
@@ -0,0 +1,103 @@
+function verificaIdade() {
+    let valor = document.getElementById('data').value
+    let saida = document.getElementById('saida')
+    var data = new Date()
+    d = data.getDay()
+    m = data.getMonth()
+    y = data.getFullYear()
+
+    ynasc = Number(valor[0] + valor[1] + valor[2] + valor[3])
+    mnasc = Number(valor[5] + valor[6])
+    dnasc = Number(valor[8] + valor[9])
+
+    if (ynasc > y && mnasc > m && dnasc > d) {
+        alert('Data invalida, data informada maior que data atual!')
+    } else {
+        let meses = somaMeses(mnasc,m)
+        let dias = somaDia(dnasc, d)
+        let anos = y - ynasc
+        let sexo = trazSexo()
+        if (mnasc > m) {
+            anos -= 1
+        }
+
+        saida.innerHTML = anos + ' anos ' + 
+        meses + ' meses ' + dias + ' dias de sexo ' + sexo
+        geraFotoDinamica(anos, sexo)
+    }
+}
+
+function somaDia(num1,num2) {
+    if (num1 > num2) {
+        return num2 + (30 - num1)
+    } else if (num2 > num1){
+        return num2 - num1
+    }else{
+        return 0
+    }
+}
+
+function somaMeses(num1,num2) {
+    if (num1 > num2) {
+        return num2 + (12 - num1)
+    } else if (num2 > num1) {
+        return (num2 + 1) - num1
+    } else {
+        return 0
+    }
+}
+
+function trazSexo() {
+    var sex = document.getElementsByName('radsex')
+
+    if(sex[0].checked) {
+        return 'Masculino'
+    } else {
+        return 'Feminino'
+    }
+}
+
+function geraFotoDinamica(y, sexo) {
+    let img = document.getElementById('foto')
+    if(y < 5 && sexo == 'Masculino') {
+        img.src = '_imagens/bebemasculino.jpg'
+    } else if (y < 5 && sexo == 'Feminino') {
+        img.src = '_imagens/bebefeminino.jpg'
+    }
+    else if(y > 5 && y < 11 && sexo == 'Masculino') {
+        img.src = '_imagens/criancamasculino.jpg'
+    } else if (y > 5 && y < 11 && sexo == 'Feminino') {
+        img.src = '_imagens/criancafeminino.jpg'
+    }
+    else if(y > 11 && y < 19 && sexo == 'Masculino') {
+        img.src = '_imagens/adolescentemasculino.jpg'
+    } else if (y > 11 && y < 19 && sexo == 'Feminino') {
+        img.src = '_imagens/adolescentefeminino.jpg'
+    }
+    else if(y > 18 && y < 30 && sexo == 'Masculino') {
+        img.src = '_imagens/adultomasculino.jpg'
+    } else if (y > 18 && y < 30 && sexo == 'Feminino') {
+        img.src = '_imagens/adultofeminino.jpg'
+    }
+    else if(y > 30 && y < 51 && sexo == 'Masculino') {
+        img.src = '_imagens/experientemasculino.jpg'
+    } else if (y > 30 && y < 51 && sexo == 'Feminino') {
+        img.src = '_imagens/experientefeminino.jpg'
+    } 
+    else if(y > 50 && y < 71 && sexo == 'Masculino') {
+        img.src = '_imagens/velhomasculino.jpg'
+    } else if (y > 50 && y < 71 && sexo == 'Feminino') {
+        img.src = '_imagens/velhofeminino.jpg'
+    } 
+    else if(y > 70 && sexo == 'Masculino') {
+        img.src = '_imagens/bemvelhomasculino.jpg'
+    } else if (y > 70 && sexo == 'Feminino') {
+        img.src = '_imagens/bemvelhofeminino.jpg'
+    } 
+    
+}
+
+function recarregaPagina() {
+    location.reload()
+    
+}
