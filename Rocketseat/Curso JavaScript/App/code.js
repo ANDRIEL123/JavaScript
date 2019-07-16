672300812843515ed596f65b5394f3ba3720cb3d
ANDRIEL123 <andrielmaicom@hotmail.com>
2019-07-16 00:55:11 -0300

    Rockeaseat JS

---
+++ Rocketseat/Curso JavaScript/App/code.js
@@ -0,0 +1,40 @@
+var listaElement = document.querySelector('#app ul')
+var inputElement = document.querySelector('input#inputadd')
+var buttonElement = document.querySelector('input#adiciona')
+
+var todos = [
+    'Fazer café',
+    'Estudando JS',
+    'Passar na faculdade amém'
+];
+
+function atualizaLista () {
+    for (i = 0; i < todos.length; i++) {
+        var outputLista = document.createElement('output')
+        outputLista.innerHTML = `<li> ${todos[i]}</li>`
+
+        var linkElement = document.createElement('a')
+        var linkText = document.createTextNode('Excluir')
+        linkElement.setAttribute('href', '#')
+
+        linkElement.appendChild(linkText)
+
+        listaElement.appendChild(outputLista)
+
+        listaElement.appendChild(linkElement)
+    }
+}
+
+buttonElement.onclick =  function  () {
+    listaElement.innerHTML = ''
+    let input = document.getElementById('inputadd').value
+    
+    todos.push(input)
+    inputElement.value = ''
+    atualizaLista ()
+}
+
+function deleteItemLista (pos) {
+    todos.splice()
+    atualizaLista()
+}
