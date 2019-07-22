d13c90558f74e36b2f1469f67e6a500fe959f792
ANDRIEL123 <andrielmaicom@hotmail.com>
2019-07-21 22:20:21 -0300

    ES6 Rocketseat

---
+++ Rocketseat/Curso JavaScript ES6+/OperacoesComArrays.js
@@ -0,0 +1,25 @@
+const arr = [1, 3, 5, 10, 15, 20]
+
+const newArr = arr.map(function(item) {
+    return item * 2
+})
+
+console.log(newArr)
+
+const sum = arr.reduce(function(total, next) {
+    return total + next
+})
+
+console.log(sum)
+
+const filter = arr.filter(function(item) {
+    return item % 2 === 0
+})
+
+console.log(filter)
+
+const find = arr.find(function(item) {
+    return item === 1
+})
+
+console.log(find)
