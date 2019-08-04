e3059ba910b14732c603fb5f1f30ca30d7a3de94
Andriel Friedrich <andrielmaicom@hotmail.com>
2019-08-04 00:27:46 -0300

    NodeJS - Usando MongoDB no conteiner docker

--- Rocketseat/NodeJS/server.js
+++ Rocketseat/NodeJS/server.js
@@ -4,13 +4,13 @@ const requireDir = require('require-dir')
 
 // Iniciando o App
 const app = express()
-
+app.use(express.json())
 // Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 
 requireDir("./src/models")
 
-// Primeira rota
+// Rotas
 app.use('/api', require("./src/routes"))
 
 app.listen(3001)
